946ef11ffe353a16b5eb30cc9f83d488417e25b6
Taihvonne Rattanavong <tdrattanavong@gmail.com>
2026-05-05 14:54:55 -0700

    Creating varibles

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -3,5 +3,19 @@ package com.pluralsight;
 public class Reservation {
 
     private String roomType;
-    private
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend){
+        this.roomType =roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+    public String getRoomType(){
+        return roomType;
+    }
+
+
+
+
 }
